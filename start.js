04ea99ed49cd3089de2364bb18486aedc770de20
Yedan Li <63386101+liyedanpdx@users.noreply.github.com>
2024-09-12 21:34:43 -0500

    mongoose

--- start.js
+++ start.js
@@ -1,8 +1,25 @@
-const app = require('./app'); //we can leave the .js off the file name in the require statement.
+require('dotenv').config();
+//we can leave the .js off the file name in the require statement.
+const mongoose = require('mongoose');
 
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+    
+mongoose.connection
+    .on('open', () => {
+        console.log('Mongoose connection open');
+    })
+    .on('error',(err) => {
+        console.log(`Connection error: ${err.message}`);
+});
+
+const app = require('./app'); 
 const server = app.listen(3000, () => {
     console.log(`Express is running on port ${server.address().port}`);
 })
+
 
 
 
